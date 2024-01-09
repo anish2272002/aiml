6e42b0947f1e62d0b802d6336ed9f1a723d39f60
Anish Kumar <anish2272002@gmail.com>
2024-01-09 23:54:16 +0530

    Add files via upload

---
+++ prog4_updated.py
@@ -0,0 +1,62 @@
+import pandas as pd
+from pprint import pprint
+from sklearn.feature_selection import mutual_info_classif
+from collections import Counter
+
+def id3(df, target_attribute, attribute_names, default_class=None):
+    cnt=Counter(x for x in df[target_attribute])
+    if len(cnt)==1:
+        return next(iter(cnt))
+    
+    elif df.empty or (not attribute_names):
+         return default_class
+
+    else:
+        gains = mutual_info_classif(df[attribute_names],df[target_attribute],discrete_features=True)
+        index_of_max=gains.tolist().index(max(gains))
+        best_attr=attribute_names[index_of_max]
+        tree={best_attr:{}}
+        remaining_attribute_names=[i for i in attribute_names if i!=best_attr]
+        
+        for attr_val, data_subset in df.groupby(best_attr):
+            subtree=id3(data_subset, target_attribute, remaining_attribute_names,default_class)
+            tree[best_attr][attr_val]=subtree
+        
+        return tree
+    
+
+df=pd.read_csv("playtennis.csv")
+
+attribute_names=df.columns.tolist()
+print("List of attribute name")
+
+attribute_names.remove("PlayTennis")
+
+for colname in df.select_dtypes("object"):
+    df[colname], _ = df[colname].factorize()
+    
+print(df)
+
+tree= id3(df,"PlayTennis", attribute_names)
+print("The tree structure")
+pprint(tree)
+
+#additional code
+def predict(query, tree, default=1):
+    for key in list(query.keys()):
+        if key in list(tree.keys()):
+            try:
+                result = tree[key][query[key]]
+            except:
+                return default
+
+            result = tree[key][query[key]] 
+            if isinstance(result,dict):
+                return predict(query,result)
+            else:
+                return result
+            
+query = {'Outlook':0,'Temperature':0,'Humidity':0,'wind':0} 
+answer = predict(query,tree)
+
+print("\n Can tennis be played for the given sample: " +str(answer))
